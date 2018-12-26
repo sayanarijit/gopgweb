6927db833c286c834fd06807f6c9bf88d3cab5f6
Arijit Basu <arijitb@juniper.net>
2018-12-26 10:22:58 -0800

    error handling

--- app/handlers.go
+++ app/handlers.go
@@ -31,18 +31,18 @@ func GenerateAPI(w http.ResponseWriter, r *http.Request) {
 			p.MaxLength, _ = strconv.Atoi(v[0])
 		case "MinLength":
 			p.MinLength, _ = strconv.Atoi(v[0])
-		case "MinCapsAlpha":
-			p.MinCapsAlpha, _ = strconv.Atoi(v[0])
-		case "MinSmallAlpha":
-			p.MinSmallAlpha, _ = strconv.Atoi(v[0])
+		case "MinUppers":
+			p.MinUppers, _ = strconv.Atoi(v[0])
+		case "MinLowers":
+			p.MinLowers, _ = strconv.Atoi(v[0])
 		case "MinDigits":
 			p.MinDigits, _ = strconv.Atoi(v[0])
 		case "MinSpclChars":
 			p.MinSpclChars, _ = strconv.Atoi(v[0])
-		case "CapsAlphaPool":
-			p.CapsAlphaPool = v[0]
-		case "SmallAlphaPool":
-			p.SmallAlphaPool = v[0]
+		case "UpperPool":
+			p.UpperPool = v[0]
+		case "LowerPool":
+			p.LowerPool = v[0]
 		case "DigitPool":
 			p.DigitPool = v[0]
 		case "SpclCharPool":
@@ -50,9 +50,14 @@ func GenerateAPI(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	password := gopassgen.Generate(p)
-
-	resp := map[string]string{"result": password}
+	resp := map[string]string{}
+	password, err := gopassgen.Generate(p)
+	if err != nil {
+		resp["error"] = err.Error()
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		resp["result"] = password
+	}
 	jsonResp, _ := json.Marshal(resp)
 
 	w.Write(jsonResp)
